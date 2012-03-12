ecdca1131b90b0c6d68b0393756c2241f12179f0
David Houghton <dhoughton@complyoninc.com>
2012-03-12 15:24:38 -0400

    added some javadocs

--- src/dfh/cli/rules/Range.java
+++ src/dfh/cli/rules/Range.java
@@ -29,50 +29,134 @@ public class Range<K extends Number> implements ValidationRule<K> {
 		this.highInc = highInc;
 	}
 
+	/**
+	 * Generates a range of the form <code>[low,high]</code>, or
+	 * <code>x &gt;= low and x &lt;= high</code>
+	 * 
+	 * @param low
+	 * @param high
+	 * @return a range of the form [low,high]
+	 */
 	public static <T extends Number> Range<T> incl(T low, T high) {
 		return new Range<T>(low, high, true, true);
 	}
 
+	/**
+	 * Generates a range of the form <code>[low,high)</code>, or
+	 * <code>x &gt;= low and x &lt; high</code>
+	 * 
+	 * @param low
+	 * @param high
+	 * @return a range of the form [low,high)
+	 */
 	public static <T extends Number> Range<T> lowIncl(T low, T high) {
 		return new Range<T>(low, high, true, false);
 	}
 
+	/**
+	 * Generates a range of the form <code>(low,high]</code>, or
+	 * <code>x &gt; low and x &lt;= high</code>
+	 * 
+	 * @param low
+	 * @param high
+	 * @return a range of the form (low,high]
+	 */
 	public static <T extends Number> Range<T> highIncl(T low, T high) {
 		return new Range<T>(low, high, false, true);
 	}
 
+	/**
+	 * Generates a range of the form <code>(low,high)</code>, or
+	 * <code>x &gt; low and x &lt; high</code>
+	 * 
+	 * @param low
+	 * @param high
+	 * @return a range of the form (low,high)
+	 */
 	public static <T extends Number> Range<T> excl(T low, T high) {
 		return new Range<T>(low, high, false, false);
 	}
 
+	/**
+	 * Generates a range of the form <code>(0,&infin;]</code>, or
+	 * <code>x &gt; low and x &lt;= &infin;</code>
+	 * 
+	 * @return a range of the form (0,&infin;]
+	 */
 	public static Range<? extends Number> positive() {
 		return highIncl(0, Double.POSITIVE_INFINITY);
 	}
 
+	/**
+	 * Generates a range of the form <code>(low,&infin;]</code>, or
+	 * <code>x &gt; low and x &lt;= &infin;</code>
+	 * 
+	 * @param low
+	 * @return a range of the form (low,&infin;]
+	 */
 	public static Range<Double> greater(double low) {
 		return highIncl(low, Double.POSITIVE_INFINITY);
 	}
 
+	/**
+	 * Generates a range of the form <code>[low,&infin;]</code>, or
+	 * <code>x &gt;= low and x &lt;= &infin;</code>
+	 * 
+	 * @param low
+	 * @return a range of the form [low,&infin;]
+	 */
 	public static Range<Double> greaterOrEq(double low) {
 		return incl(low, Double.POSITIVE_INFINITY);
 	}
 
+	/**
+	 * Generates a range of the form <code>[-&infin;,0)</code>, or
+	 * <code>x &gt;= -&infin; and x &lt; 0</code>
+	 * 
+	 * @return a range of the form [-&infin;,0)
+	 */
 	public static Range<? extends Number> negative() {
 		return lowIncl(Double.NEGATIVE_INFINITY, 0);
 	}
 
+	/**
+	 * Generates a range of the form <code>[-&infin;,high)</code>, or
+	 * <code>x &gt;= low and x &lt; high</code>
+	 * 
+	 * @param high
+	 * @return a range of the form [-&infin;,high)
+	 */
 	public static Range<Double> less(double high) {
 		return lowIncl(Double.NEGATIVE_INFINITY, high);
 	}
 
+	/**
+	 * Generates a range of the form <code>[-&infin;,high]</code>, or
+	 * <code>x &gt;= -&infin; and x &lt;= high</code>
+	 * 
+	 * @param high
+	 * @return a range of the form [-&infin;,high]
+	 */
 	public static Range<Double> lessOrEq(double high) {
 		return incl(Double.NEGATIVE_INFINITY, high);
 	}
 
+	/**
+	 * Generates a range of the form <code>[0,&infin;]</code>, or
+	 * <code>x &gt;= 0 and x &lt;= &infin;</code>
+	 * 
+	 * @return a range of the form [0,&infin;]
+	 */
 	public static Range<? extends Number> nonNegative() {
 		return incl(0, Double.POSITIVE_INFINITY);
 	}
 
+	/**
+	 * Generates a range of the form <code>[-&infin;,0]</code>, or
+	 * <code>x &gt;= -&infin; and x &lt;= 0</code>
+	 * 
+	 * @return a range of the form [-&infin;,0]
+	 */
 	public static Range<? extends Number> nonPositive() {
 		return incl(Double.NEGATIVE_INFINITY, 0);
 	}
